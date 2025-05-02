bd103c30b78f1fb3c72e28bcf4a8b8ab60af909b
ARafaykhalid <abdulrafaykhalidjameel@gmail.com>
2025-05-02 15:34:09 +0500

    Hero+About

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+"use client"
+import About from "@/components/About";
+import Hero from "@/components/Hero";
+
+const App = () => {
+  return (
+    <main className="relative min-h-screen">
+      <Hero />
+      <About />
+    </main>
+  );
+};
+
+export default App;
